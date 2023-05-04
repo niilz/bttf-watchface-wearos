fac2b549c772fe6904f1c01d769520bd2fdd9129
nilSinger <niilz@outlook.de>
2023-05-04 13:41:37 +0200

    Remove unused Canvas import

--- app/src/main/java/de/niilz/wearos/watchface/bttf/BttfWatchface.kt
+++ app/src/main/java/de/niilz/wearos/watchface/bttf/BttfWatchface.kt
@@ -1,6 +1,5 @@
 package de.niilz.wearos.watchface.bttf
 
-import android.graphics.Canvas
 import android.support.wearable.watchface.CanvasWatchFaceService
 
 class BttfWatchface : CanvasWatchFaceService() {
