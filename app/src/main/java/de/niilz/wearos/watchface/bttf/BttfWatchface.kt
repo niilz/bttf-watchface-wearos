fc799ce168b34b615e97ff9abfd18dd654b1be0b
nilSinger <niilz@outlook.de>
2023-05-04 10:38:26 +0200

    Initial commit
     - added dependencies for watchface
     - added basic stub class file
     - added xml resource file
     - added manifest entries up until but not with pictures

---
+++ app/src/main/java/de/niilz/wearos/watchface/bttf/BttfWatchface.kt
@@ -0,0 +1,9 @@
+package de.niilz.wearos.watchface.bttf
+
+import android.graphics.Canvas
+import android.support.wearable.watchface.CanvasWatchFaceService
+
+class BttfWatchface : CanvasWatchFaceService() {
+
+    inner class Engine : CanvasWatchFaceService.Engine() {}
+}
