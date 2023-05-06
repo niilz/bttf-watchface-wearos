d4de0ae798e79c6ca7b16d549b59bc5214c1a4bc
nilSinger <niilz@outlook.de>
2023-05-06 18:23:05 +0200

    Add basics to scale and draw the background

--- app/src/main/java/de/niilz/wearos/watchface/bttf/BttfWatchface.kt
+++ app/src/main/java/de/niilz/wearos/watchface/bttf/BttfWatchface.kt
@@ -1,8 +1,53 @@
 package de.niilz.wearos.watchface.bttf
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Rect
 import android.support.wearable.watchface.CanvasWatchFaceService
+import android.view.SurfaceHolder
 
 class BttfWatchface : CanvasWatchFaceService() {
 
-    inner class Engine : CanvasWatchFaceService.Engine() {}
+    override fun onCreateEngine(): CanvasWatchFaceService.Engine {
+        return Engine()
+    }
+
+    inner class Engine : CanvasWatchFaceService.Engine() {
+        private lateinit var backgroundBitmap: Bitmap
+
+        override fun onCreate(holder: SurfaceHolder?) {
+            super.onCreate(holder)
+            initializeBackground()
+        }
+
+        override fun onDraw(canvas: Canvas, bounds: Rect?) {
+            drawBackground(canvas)
+        }
+
+        private fun drawBackground(canvas: Canvas) {
+            canvas.drawBitmap(backgroundBitmap, 0f, 0f, null)
+        }
+
+        override fun onSurfaceChanged(
+            holder: SurfaceHolder?,
+            format: Int,
+            width: Int,
+            height: Int
+        ) {
+            super.onSurfaceChanged(holder, format, width, height)
+
+            val scale = width.toFloat() / backgroundBitmap.width.toFloat()
+            backgroundBitmap = Bitmap.createScaledBitmap(
+                backgroundBitmap,
+                (backgroundBitmap.width * scale).toInt(),
+                (backgroundBitmap.height * scale).toInt(),
+                true
+            )
+        }
+
+        private fun initializeBackground() {
+            backgroundBitmap = BitmapFactory.decodeResource(resources, R.drawable.bg)
+        }
+    }
 }
