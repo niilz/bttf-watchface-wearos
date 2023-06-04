eef1584824716777ed3c1923f1ba641dbd7642f1
niilz <niilz@outlook.de>
2023-06-04 20:40:55 +0200

    Make Slot be aware of the label and draw it

--- app/src/main/java/de/niilz/wearos/watchface/bttf/Slot.kt
+++ app/src/main/java/de/niilz/wearos/watchface/bttf/Slot.kt
@@ -9,21 +9,27 @@ import androidx.core.content.ContextCompat
 class Slot(
     context: Context,
     private val items: List<DrawableItem>,
+    private val label: DrawableItem,
     private val left: Float,
     private val top: Float,
     private val gap: Float,
 ) {
     private val background = ContextCompat.getColor(context, R.color.slot_bg_color)
 
-    private var width = items.size * items[0].getWidth() + items.size * gap
+    private var slotWidth = items.size * items[0].getWidth() + items.size * gap
 
     fun draw(canvas: Canvas) {
         var cursor = left + gap
-        val marginTop = top + gap
+        var marginTop = top + gap
 
-        val right = left + width + (2 * gap)
-        val bottom = top + items[0].getHeight() + (2 * gap)
-        val bgRect = Rect(left.toInt(), top.toInt(), right.toInt(), bottom.toInt())
+        // TODO: add XXX to 'cursor' so that label is center
+        label.draw(canvas, cursor, marginTop)
+        val valuesTop = marginTop + label.getHeight() + gap
+
+
+        val right = left + slotWidth + (2 * gap)
+        val bottom = valuesTop + items[0].getHeight() + (2 * gap)
+        val bgRect = Rect(left.toInt(), valuesTop.toInt(), right.toInt(), bottom.toInt())
         val rectPaint = Paint().apply {
             color = background
             style = Paint.Style.FILL
@@ -31,12 +37,12 @@ class Slot(
         canvas.drawRect(bgRect, rectPaint)
 
         for (item in items) {
-            item.draw(canvas, cursor, marginTop)
+            item.draw(canvas, cursor, valuesTop + gap)
             cursor += item.getWidth() + gap
         }
     }
 
     fun getWidth(): Float {
-        return width
+        return slotWidth
     }
 }
