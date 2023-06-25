b5e8cfa98646f8aaf2046f95fb991bc1f4dd7d9c
niilz <niilz@outlook.de>
2023-06-25 16:26:57 +0200

    Add new ComplicationConfigActivity class and basic compli-config in manifest

---
+++ app/src/main/java/de/niilz/wearos/watchface/bttf/ComplicationConfigActivity.kt
@@ -0,0 +1,10 @@
+package de.niilz.wearos.watchface.bttf
+
+import android.app.Activity
+import android.os.Bundle
+
+class ComplicationConfigActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
