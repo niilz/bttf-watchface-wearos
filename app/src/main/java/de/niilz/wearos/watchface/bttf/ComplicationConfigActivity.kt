9731a78365d1d0f4de6c306497924a01355cbce5
niilz <niilz@outlook.de>
2023-06-25 19:39:08 +0200

    Add a very basic component-activity with TODO-text for settings

--- app/src/main/java/de/niilz/wearos/watchface/bttf/ComplicationConfigActivity.kt
+++ app/src/main/java/de/niilz/wearos/watchface/bttf/ComplicationConfigActivity.kt
@@ -1,10 +1,32 @@
 package de.niilz.wearos.watchface.bttf
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
-class ComplicationConfigActivity : Activity() {
+class ComplicationConfigActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val myMod = Modifier
+            .fillMaxWidth()
+            .fillMaxSize()
+        setContent {
+            Column(
+                modifier = myMod,
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("TODO:", color = Color.White)
+                Text("Settings Activity", color = Color.White)
+            }
+        }
     }
 }
