fa2be37c312e7d9961c2f7f255e80c09d574c34b
niilz <niilz@outlook.de>
2023-06-28 22:11:24 +0200

    upgrade/refactor to use newer WatchFaceService and WatchFaceRender

--- app/src/main/java/de/niilz/wearos/watchface/bttf/BttfWatchface.kt
+++ app/src/main/java/de/niilz/wearos/watchface/bttf/BttfWatchface.kt
@@ -1,167 +1,28 @@
 package de.niilz.wearos.watchface.bttf
 
-import android.graphics.*
-import android.graphics.drawable.Drawable
-import android.support.wearable.watchface.CanvasWatchFaceService
 import android.view.SurfaceHolder
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.toBitmap
-import java.time.LocalDateTime
-import kotlin.math.sqrt
+import androidx.wear.watchface.*
+import androidx.wear.watchface.style.CurrentUserStyleRepository
 
-class BttfWatchface : CanvasWatchFaceService() {
+class BttfWatchface : WatchFaceService() {
 
-    override fun onCreateEngine(): CanvasWatchFaceService.Engine {
-        return Engine()
+    override suspend fun createWatchFace(
+        surfaceHolder: SurfaceHolder,
+        watchState: WatchState,
+        complicationSlotsManager: ComplicationSlotsManager,
+        currentUserStyleRepository: CurrentUserStyleRepository
+    ): WatchFace {
+
+        val renderer = WatchFaceRenderer(
+            applicationContext,
+            resources,
+            surfaceHolder,
+            watchState,
+            complicationSlotsManager,
+            currentUserStyleRepository,
+            CanvasType.HARDWARE
+        )
+        return WatchFace(WatchFaceType.DIGITAL, renderer)
     }
 
-    inner class Engine : CanvasWatchFaceService.Engine() {
-        private lateinit var backgroundBitmap: Bitmap
-        private lateinit var numbers: List<Bitmap>
-
-        private val shadowRadius = resources.getDimension(R.dimen.shadow_radius)
-        private val numberWidth = resources.getDimension(R.dimen.number_width)
-        private val numberHeight = resources.getDimension(R.dimen.number_height)
-        private val leftMarginScalar = resources.getDimension(R.dimen.top_margin_scalar)
-        private val topMarginScalar = resources.getDimension(R.dimen.left_margin_scalar)
-        private var initialNumberWidth = 0.0f
-        private var initialNumberHeight = 0.0f
-        private val gap = resources.getDimension(R.dimen.gap)
-
-        private lateinit var numberPaint: Paint
-
-        private var topLeftX = 0f
-        private var topLeftY = 0f
-        private var radius = 0f
-        private var canvasInnerWidthOrHeight = 0f
-        private var firstRowTopMargin = 0f
-        private var firstRowLeftMargin = 0f
-
-        private lateinit var drawService: DrawService;
-
-        override fun onCreate(holder: SurfaceHolder?) {
-            //println("*** onCreate ***")
-            super.onCreate(holder)
-            initializeBackground()
-            initializeNumbers()
-            drawService = DrawService(context = applicationContext, numberBitmaps = numbers)
-        }
-
-        override fun onDraw(canvas: Canvas, bounds: Rect?) {
-            //println("*** onDraw ***")
-            if (drawService.canvas == null) {
-                drawService.canvas = canvas
-            }
-            drawBackground(canvas)
-            drawSlots()
-        }
-
-        override fun onVisibilityChanged(visible: Boolean) {
-            //println("*** onVisibilityChanged ***")
-            super.onVisibilityChanged(visible)
-            invalidate()
-        }
-
-        private fun drawSlots() {
-            //println("*** drawSlots ***")
-            val now = LocalDateTime.now()
-            val top = topLeftY + firstRowTopMargin
-
-            // FIRST-ROW
-            var leftStart = topLeftX + firstRowLeftMargin
-
-            // Month-Name Slot
-            val monthSlotData = TextSlotMetadata(
-                "MONTH",
-                now.month.toString().substring(0, 3),
-                gap
-            )
-
-            // TODO: Maybe globally define margin
-            val margin = 2 * gap
-
-            // Day Slot
-            val dayNums = MapperUtil.mapTwoDigitNumToInts(now.dayOfMonth)
-            val daySlotData = BitmapSlotMetadata("DAY", dayNums, margin)
-
-            // Year Slot
-            val yearNums = MapperUtil.mapYearToInts(now.year)
-            val yearSlotData = BitmapSlotMetadata("YEAR", yearNums, margin)
-
-            // Hour Slot
-            val hourNums = MapperUtil.mapTwoDigitNumToInts(now.hour)
-            val hourSlotData = BitmapSlotMetadata("HOUR", hourNums, 2 * margin)
-
-            // TODO: Draw am-pm dots (Dot-Slot / drwable Item)
-
-            // Minute Slot
-            val minuteNums = MapperUtil.mapTwoDigitNumToInts(now.minute)
-            val minuteSlotData = BitmapSlotMetadata("MIN", minuteNums, 0f)
-
-            drawService.drawRow(
-                leftStart,
-                top,
-                listOf(monthSlotData, daySlotData, yearSlotData, hourSlotData, minuteSlotData)
-            )
-
-        }
-
-        private fun drawBackground(canvas: Canvas) {
-            //println("*** start drawBackground ***")
-            canvas.drawBitmap(backgroundBitmap, topLeftX, topLeftY, null)
-            //println("*** end drawBackground ***")
-        }
-
-        override fun onSurfaceChanged(
-            holder: SurfaceHolder?,
-            format: Int,
-            width: Int,
-            height: Int
-        ) {
-            super.onSurfaceChanged(holder, format, width, height)
-            //println("*** onSurfaceChanged ***")
-
-            calcValues(width.toFloat(), height.toFloat())
-            val backgroundScale = canvasInnerWidthOrHeight / backgroundBitmap.width.toFloat()
-            backgroundBitmap =
-                MapperUtil.scaleBitmap(backgroundBitmap, backgroundScale, backgroundScale)
-
-            drawService.updateNumbers(
-                canvasInnerWidthOrHeight,
-                initialNumberWidth,
-                initialNumberHeight,
-                18,
-                10
-            )
-
-        }
-
-        private fun initializeBackground() {
-            //println("*** initializeBackground ***")
-            backgroundBitmap = BitmapFactory.decodeResource(resources, R.drawable.bg)
-        }
-
-        private fun calcValues(width: Float, height: Float) {
-            //println("*** calcValues ***")
-            radius = width / 2f
-            canvasInnerWidthOrHeight = sqrt(2f) * radius;
-            val (x, y) = MapperUtil.calcTopLeftCornerOnCirlce(width, height)
-            topLeftX = x
-            topLeftY = y
-            firstRowTopMargin = canvasInnerWidthOrHeight * topMarginScalar
-            firstRowLeftMargin = canvasInnerWidthOrHeight * leftMarginScalar
-        }
-
-        private fun initializeNumbers() {
-            //println("*** initializeNumbers ***")
-            fun getDrawable(idx: Int): Drawable? {
-                val id = resources.getIdentifier("number_$idx", "drawable", packageName)
-                return ContextCompat.getDrawable(applicationContext, id)
-            }
-            numbers = (0..9).map { getDrawable(it)!!.toBitmap() }.toList()
-            val firstNumber = numbers[0]
-            initialNumberWidth = firstNumber.width.toFloat()
-            initialNumberHeight = firstNumber.height.toFloat()
-        }
-    }
 }
---
+++ app/src/main/java/de/niilz/wearos/watchface/bttf/WatchFaceRenderer.kt
@@ -0,0 +1,208 @@
+package de.niilz.wearos.watchface.bttf
+
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import android.view.SurfaceHolder
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import androidx.wear.watchface.ComplicationSlotsManager
+import androidx.wear.watchface.Renderer
+import androidx.wear.watchface.WatchState
+import androidx.wear.watchface.style.CurrentUserStyleRepository
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
+import kotlin.math.sqrt
+
+private const val FRAME_PERIOD_MS_DEFAULT = 16L
+
+class WatchFaceRenderer(
+    private val context: Context,
+    private val resources: Resources,
+    surfaceHolder: SurfaceHolder,
+    watchState: WatchState,
+    private val complicationSlotsManager: ComplicationSlotsManager,
+    currentUserStyleRepository: CurrentUserStyleRepository,
+    canvasType: Int
+) : Renderer.CanvasRenderer2<WatchFaceRenderer.DigitalSharedAssets>(
+    surfaceHolder,
+    currentUserStyleRepository,
+    watchState,
+    canvasType,
+    FRAME_PERIOD_MS_DEFAULT,
+    clearWithBackgroundTintBeforeRenderingHighlightLayer = false
+) {
+    class DigitalSharedAssets : SharedAssets {
+        override fun onDestroy() {
+        }
+    }
+
+    private lateinit var backgroundBitmap: Bitmap
+    private lateinit var numbers: List<Bitmap>
+
+    private val shadowRadius = resources.getDimension(R.dimen.shadow_radius)
+    private val numberWidth = resources.getDimension(R.dimen.number_width)
+    private val numberHeight = resources.getDimension(R.dimen.number_height)
+    private val leftMarginScalar = resources.getDimension(R.dimen.top_margin_scalar)
+    private val topMarginScalar = resources.getDimension(R.dimen.left_margin_scalar)
+    private var initialNumberWidth = 0.0f
+    private var initialNumberHeight = 0.0f
+    private val gap = resources.getDimension(R.dimen.gap)
+
+    private lateinit var numberPaint: Paint
+
+    private var topLeftX = 0f
+    private var topLeftY = 0f
+    private var radius = 0f
+    private var canvasInnerWidthOrHeight = 0f
+    private var firstRowTopMargin = 0f
+    private var firstRowLeftMargin = 0f
+    private var areValuesInit = false
+
+    private var drawService: DrawService;
+
+    init {
+        println("*** init ***")
+        initializeBackground()
+        initializeNumbers()
+        drawService = DrawService(context = context, numberBitmaps = numbers)
+    }
+
+    override suspend fun createSharedAssets(): DigitalSharedAssets {
+        return DigitalSharedAssets()
+    }
+
+    override fun render(
+        canvas: Canvas,
+        bounds: Rect,
+        zonedDateTime: ZonedDateTime,
+        sharedAssets: DigitalSharedAssets
+    ) {
+        println("*** onDraw ***")
+        if (drawService.canvas == null) {
+            drawService.canvas = canvas
+        }
+        // TODO: Call update somewhere usefull  not on every render
+        if (!areValuesInit) {
+            oldOnSurfaceChanged(canvas.width, canvas.height)
+            areValuesInit = true
+        }
+        drawBackground(canvas)
+        drawSlots()
+    }
+
+    override fun renderHighlightLayer(
+        canvas: Canvas,
+        bounds: Rect,
+        zonedDateTime: ZonedDateTime,
+        sharedAssets: DigitalSharedAssets
+    ) {
+        println("TODO: Can render hightlight layer here")
+    }
+
+    private fun drawSlots() {
+        println("*** drawSlots ***")
+        val now = LocalDateTime.now()
+        val top = topLeftY + firstRowTopMargin
+
+        // FIRST-ROW
+        var leftStart = topLeftX + firstRowLeftMargin
+
+        // Month-Name Slot
+        val monthSlotData = TextSlotMetadata(
+            "MONTH",
+            now.month.toString().substring(0, 3),
+            gap
+        )
+
+        // TODO: Maybe globally define margin
+        val margin = 2 * gap
+
+        // Day Slot
+        val dayNums = MapperUtil.mapTwoDigitNumToInts(now.dayOfMonth)
+        val daySlotData = BitmapSlotMetadata("DAY", dayNums, margin)
+
+        // Year Slot
+        val yearNums = MapperUtil.mapYearToInts(now.year)
+        val yearSlotData = BitmapSlotMetadata("YEAR", yearNums, margin)
+
+        // Hour Slot
+        val hourNums = MapperUtil.mapTwoDigitNumToInts(now.hour)
+        val hourSlotData = BitmapSlotMetadata("HOUR", hourNums, 2 * margin)
+
+        // TODO: Draw am-pm dots (Dot-Slot / drwable Item)
+
+        // Minute Slot
+        val minuteNums = MapperUtil.mapTwoDigitNumToInts(now.minute)
+        val minuteSlotData = BitmapSlotMetadata("MIN", minuteNums, 0f)
+
+        drawService.drawRow(
+            leftStart,
+            top,
+            listOf(monthSlotData, daySlotData, yearSlotData, hourSlotData, minuteSlotData)
+        )
+
+    }
+
+    private fun drawBackground(canvas: Canvas) {
+        println("*** start drawBackground ***")
+        canvas.drawBitmap(backgroundBitmap, topLeftX, topLeftY, null)
+        println("*** end drawBackground ***")
+    }
+
+    fun oldOnSurfaceChanged(
+        width: Int,
+        height: Int
+    ) {
+        println("*** onSurfaceChanged ***")
+
+        calcValues(width.toFloat(), height.toFloat())
+        val backgroundScale = canvasInnerWidthOrHeight / backgroundBitmap.width.toFloat()
+        backgroundBitmap =
+            MapperUtil.scaleBitmap(backgroundBitmap, backgroundScale, backgroundScale)
+
+        drawService.updateNumbers(
+            canvasInnerWidthOrHeight,
+            initialNumberWidth,
+            initialNumberHeight,
+            18,
+            10
+        )
+
+    }
+
+    private fun initializeBackground() {
+        println("*** initializeBackground ***")
+        backgroundBitmap = BitmapFactory.decodeResource(resources, R.drawable.bg)
+    }
+
+    // TODO: Do it somewhere useful
+    private fun calcValues(width: Float, height: Float) {
+        println("*** calcValues ***")
+        radius = width / 2f
+        canvasInnerWidthOrHeight = sqrt(2f) * radius;
+        val (x, y) = MapperUtil.calcTopLeftCornerOnCirlce(width, height)
+        topLeftX = x
+        topLeftY = y
+        firstRowTopMargin = canvasInnerWidthOrHeight * topMarginScalar
+        firstRowLeftMargin = canvasInnerWidthOrHeight * leftMarginScalar
+    }
+
+    private fun initializeNumbers() {
+        println("*** initializeNumbers ***")
+        fun getDrawable(idx: Int): Drawable? {
+            val id = resources.getIdentifier("number_$idx", "drawable", context.packageName)
+            return ContextCompat.getDrawable(context, id)
+        }
+        numbers = (0..9).map { getDrawable(it)!!.toBitmap() }.toList()
+        val firstNumber = numbers[0]
+        initialNumberWidth = firstNumber.width.toFloat()
+        initialNumberHeight = firstNumber.height.toFloat()
+    }
+
+}
