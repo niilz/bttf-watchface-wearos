9992dbe0a2716735ad15ff22651c2e40ab14e620
niilz <niilz@outlook.de>
2023-07-29 19:35:19 +0200

    Add buttons that are actually callabel

--- app/src/main/java/de/niilz/wearos/watchface/bttf/editor/ComplicationConfigActivity.kt
+++ app/src/main/java/de/niilz/wearos/watchface/bttf/editor/ComplicationConfigActivity.kt
@@ -8,14 +8,25 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
 class ComplicationConfigActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /*
+        lifecycleScope.launch(Dispatchers.Main.immediate) {
+            state.complicationData.collect { complicationData ->
+                println("### collected complication data")
+            }
+        }
+         */
+
         val myMod = Modifier
             .background(color = Color.Black)
             .fillMaxWidth()
@@ -28,7 +39,20 @@ class ComplicationConfigActivity : ComponentActivity() {
             ) {
                 Text("TODO:", color = Color.White)
                 Text("Settings Activity", color = Color.White)
+                Button(onClick = { chooseComplication(0) }) {
+                }
+                Button(onClick = { chooseComplication(1) }) {
+                }
+                Button(onClick = { chooseComplication(2) }) {
+                }
             }
         }
+    }
+
+    private fun chooseComplication(id: Int) {
+        println("Clicked complication button")
+
+        // TODO: have a state holder class
+        //state.setComplication(42 + id)
     }
 }
