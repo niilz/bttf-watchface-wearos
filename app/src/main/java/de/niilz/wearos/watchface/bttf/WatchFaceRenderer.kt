ee52497d10441bbff5add3809f6f62a8ea723623
niilz <niilz@outlook.de>
2023-08-03 13:18:14 +0200

    Invert the percent-sign logic

--- app/src/main/java/de/niilz/wearos/watchface/bttf/WatchFaceRenderer.kt
+++ app/src/main/java/de/niilz/wearos/watchface/bttf/WatchFaceRenderer.kt
@@ -216,7 +216,7 @@ class WatchFaceRenderer(
             }.map {
                 val label = MapperUtil.classNameToCamelCaseParts(it.first).first().uppercase()
                 val slotValues = parseNumbersAndTexts(it.second).toMutableList()
-                if (label == "BATTERY" && (slotValues.last() as? TextVal)?.text?.endsWith("%") == true) {
+                if (label == "BATTERY" && (slotValues.last() as? TextVal)?.text?.endsWith("%") == false) {
                     slotValues.add(TextVal("%"))
                 }
                 SlotMetadata(label, valueColor, margin, slotValues)
