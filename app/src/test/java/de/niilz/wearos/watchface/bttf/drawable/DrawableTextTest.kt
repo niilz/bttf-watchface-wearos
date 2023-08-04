11d8bbc156b08ac53a81d67964c3414bea4e10a8
niilz <niilz@outlook.de>
2023-08-04 23:25:38 +0200

    Add test dependencies and a test-stub

---
+++ app/src/test/java/de/niilz/wearos/watchface/bttf/drawable/DrawableTextTest.kt
@@ -0,0 +1,12 @@
+package de.niilz.wearos.watchface.bttf.drawable
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class DrawableTextTest {
+
+    @Test
+    fun getRawCharHeight() {
+        Assertions.assertTrue(true)
+    }
+}
