4a7e6a0679e46ce1144282b78e5e73d9ddfa6318
niilz <niilz@outlook.de>
2023-08-06 17:48:15 +0200

    Move the heartbeat-listener logic to a HeartBeatService-Class

--- app/src/main/java/de/niilz/wearos/watchface/bttf/service/HeartBeatService.kt
+++ app/src/main/java/de/niilz/wearos/watchface/bttf/service/HeartBeatService.kt
@@ -1,18 +1,43 @@
 package de.niilz.wearos.watchface.bttf.service
 
+import android.content.Context
 import android.util.Log
 import androidx.health.services.client.HealthServices
-import androidx.health.services.client.PassiveListenerService
+import androidx.health.services.client.PassiveListenerCallback
+import androidx.health.services.client.clearPassiveListenerCallback
 import androidx.health.services.client.data.DataPointContainer
 import androidx.health.services.client.data.DataType
+import androidx.health.services.client.data.PassiveListenerConfig
+import androidx.health.services.client.getCapabilities
+import de.niilz.wearos.watchface.bttf.TAG
 
-class HeartBeatService : PassiveListenerService() {
+class HeartBeatService(context: Context) {
 
-    val healthServiceClient = HealthServices.getClient(applicationContext)
+    private val healthClient = HealthServices.getClient(context)
+    private val passiveMonitoringClient = healthClient.passiveMonitoringClient
 
-    override fun onNewDataPointsReceived(dataPoints: DataPointContainer) {
-        dataPoints.getData(DataType.HEART_RATE_BPM).forEach {
-            Log.i("listener", "$it")
+    init {
+        val dataConfig = PassiveListenerConfig.builder()
+            .setDataTypes(setOf(DataType.HEART_RATE_BPM))
+            .build()
+        val listenerCallback = object : PassiveListenerCallback {
+            override fun onNewDataPointsReceived(dataPoints: DataPointContainer) {
+                val heartRates = dataPoints.getData(DataType.HEART_RATE_BPM)
+                for (heartRate in heartRates) {
+                    Log.i(TAG, "HeartRate: ${heartRate.value}")
+                }
+            }
         }
+        passiveMonitoringClient.setPassiveListenerCallback(dataConfig, listenerCallback)
+    }
+
+    suspend fun hasHeartBeatCapabilities(): Boolean {
+        val capabilities = passiveMonitoringClient.getCapabilities()
+        return DataType.HEART_RATE_BPM in capabilities.supportedDataTypesPassiveMonitoring
+    }
+
+    suspend fun unregisterHeartBeatReceive() {
+        Log.i(TAG, "Unregister HeartBeatListener")
+        passiveMonitoringClient.clearPassiveListenerCallback()
     }
 }
