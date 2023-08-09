c8117cb2bdd21ba985f9ab8fbdad2da60dccd60b
Haberstroh <Nils.Haberstroh@computacenter.com>
2023-08-09 17:25:44 +0200

    show how maybe the value setting could happen if a could be accessed without crashing

--- app/src/main/java/de/niilz/wearos/watchface/bttf/service/HeartBeatService.kt
+++ app/src/main/java/de/niilz/wearos/watchface/bttf/service/HeartBeatService.kt
@@ -2,6 +2,7 @@ package de.niilz.wearos.watchface.bttf.service
 
 import android.content.Context
 import android.util.Log
+import androidx.datastore.preferences.core.edit
 import androidx.health.services.client.HealthServices
 import androidx.health.services.client.PassiveListenerCallback
 import androidx.health.services.client.clearPassiveListenerCallback
@@ -10,11 +11,15 @@ import androidx.health.services.client.data.DataType
 import androidx.health.services.client.data.PassiveListenerConfig
 import androidx.health.services.client.getCapabilities
 import de.niilz.wearos.watchface.bttf.TAG
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class HeartBeatService(context: Context) {
 
     private val healthClient = HealthServices.getClient(context)
     private val passiveMonitoringClient = healthClient.passiveMonitoringClient
+    private val scope = CoroutineScope(Dispatchers.Main.immediate)
 
     init {
         val dataConfig = PassiveListenerConfig.builder()
@@ -23,11 +28,17 @@ class HeartBeatService(context: Context) {
         val listenerCallback = object : PassiveListenerCallback {
             override fun onNewDataPointsReceived(dataPoints: DataPointContainer) {
                 val heartRates = dataPoints.getData(DataType.HEART_RATE_BPM)
-                for (heartRate in heartRates) {
-                    Log.i(TAG, "HeartRate: ${heartRate.value}")
+                    for (heartRate in heartRates) {
+                        //Log.i(TAG, "HeartRate: ${preferences[HEART_RATE_PREF_KEY]}")
+                        Log.i(TAG, "HeartRate: ${heartRate.value}")
+                        /*
+                        context.dataStore.edit { preferences ->
+                            preferences[HEART_RATE_PREF_KEY] = heartRate.value.toInt()
+                        }
+                         */
+                    }
                 }
             }
-        }
         passiveMonitoringClient.setPassiveListenerCallback(dataConfig, listenerCallback)
     }
 
