548a6fa6191f8a85a104d92f7bb1136777dc8af3
Haberstroh <Nils.Haberstroh@computacenter.com>
2023-08-14 14:07:05 +0200

    Abstract button-composables and add preview

--- app/src/main/java/de/niilz/wearos/watchface/bttf/editor/ComplicationConfigActivity.kt
+++ app/src/main/java/de/niilz/wearos/watchface/bttf/editor/ComplicationConfigActivity.kt
@@ -9,17 +9,25 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import androidx.wear.watchface.editor.EditorSession
 import de.niilz.wearos.watchface.bttf.TAG
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 class ComplicationConfigActivity : ComponentActivity() {
 
@@ -45,27 +53,8 @@ class ComplicationConfigActivity : ComponentActivity() {
         }
          */
 
-        val myMod = Modifier
-            .background(color = Color.Black)
-            .fillMaxWidth()
-            .fillMaxSize()
         setContent {
-            Column(
-                modifier = myMod,
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text("TODO:", color = Color.White)
-                Text("Settings Activity", color = Color.White)
-                Button(onClick = { chooseComplication(0) }) {
-                }
-                Button(onClick = { chooseComplication(1) }) {
-                }
-                Button(onClick = { chooseComplication(2) }) {
-                }
-                Button(onClick = { chooseComplication(3) }) {
-                }
-            }
+            complicationSelectRow(chooseComplication = ::chooseComplication, ids = listOf(0, 1, 2, 3))
         }
     }
 
@@ -77,4 +66,39 @@ class ComplicationConfigActivity : ComponentActivity() {
             editorSession.openComplicationDataSourceChooser(42 + id)
         }
     }
+}
+
+@Composable
+fun complicationSelectRow(chooseComplication: (Int) -> Unit, ids: List<Int>) {
+    val myMod = Modifier
+        .background(color = Color.Black)
+        .fillMaxWidth()
+        .fillMaxSize()
+
+    Column(
+        modifier = myMod,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Chose Complications", color = Color.White)
+        Row {
+            for (id in ids) {
+                complicationChooserButton(selectComplicationHandler = chooseComplication, id = id)
+            }
+        }
+    }
+}
+
+@Composable
+fun complicationChooserButton(selectComplicationHandler: (Int) -> Unit, id: Int) {
+    val buttonColors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+    Button(onClick = { selectComplicationHandler(id) }, colors = buttonColors) {}
+}
+
+
+@Preview(device = Devices.WEAR_OS_LARGE_ROUND)
+@Composable
+fun preview() {
+    val dummyCallback: (Int) -> Unit = { id -> println("The ID is: $id") }
+    complicationSelectRow(dummyCallback, listOf(0, 1, 2, 3))
 }
