1b956fc5202d3a645456767cc3031567706f71a3
niilz <niilz@outlook.de>
2023-08-15 07:31:28 +0200

    Update UI in config activity (misses state)

--- app/src/main/java/de/niilz/wearos/watchface/bttf/editor/ComplicationConfigActivity.kt
+++ app/src/main/java/de/niilz/wearos/watchface/bttf/editor/ComplicationConfigActivity.kt
@@ -19,7 +19,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -36,118 +36,132 @@ import kotlinx.coroutines.launch
 
 class ComplicationConfigActivity : ComponentActivity() {
 
-  private lateinit var editorSession: EditorSession
-  private var complicationCount = 0
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+    private lateinit var editorSession: EditorSession
 
-    lifecycleScope.launch {
-      editorSession =
-        EditorSession.createOnWatchEditorSession(this@ComplicationConfigActivity)
+    // TODO: have real state somewhere
+    private var complicationCount = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-      val hasHeartRatePermission =
-        checkSelfPermission(Manifest.permission.BODY_SENSORS) == PackageManager.PERMISSION_GRANTED
-      if (!hasHeartRatePermission) {
-        requestPermissions(arrayOf(Manifest.permission.BODY_SENSORS), 1)
-      }
+        lifecycleScope.launch {
+            editorSession =
+                EditorSession.createOnWatchEditorSession(this@ComplicationConfigActivity)
+
+            val hasHeartRatePermission =
+                checkSelfPermission(Manifest.permission.BODY_SENSORS) == PackageManager.PERMISSION_GRANTED
+            if (!hasHeartRatePermission) {
+                requestPermissions(arrayOf(Manifest.permission.BODY_SENSORS), 1)
+            }
+        }
+
+        setContent {
+            complicationSelectRow(
+                chooseComplication = ::selectComplicationHandler,
+                ::incrementComplicationCount,
+                ::decrementComplicationCount,
+                maxId = complicationCount
+            )
+        }
     }
 
-    setContent {
-      complicationSelectRow(
-        chooseComplication = ::selectComplicationHandler,
-        { complicationCount += 1 },
-        { complicationCount -= 1 },
-        ids = complicationCount
-      )
+    private fun incrementComplicationCount() {
+        complicationCount += 1
+        Log.d(TAG, "ComplicationCount: $complicationCount")
     }
-  }
 
-  private fun selectComplicationHandler(id: Int) {
-    Log.i(TAG, "Selected Complication, ID: $id")
-
-    // TODO: have a state holder class
-    lifecycleScope.launch {
-      editorSession.openComplicationDataSourceChooser(42 + id)
+    private fun decrementComplicationCount() {
+        complicationCount -= 1
+        Log.d(TAG, "ComplicationCount: $complicationCount")
     }
-  }
+
+    private fun selectComplicationHandler(id: Int) {
+        Log.i(TAG, "Selected Complication, ID: $id")
+
+        // TODO: have a state holder class
+        lifecycleScope.launch {
+            editorSession.openComplicationDataSourceChooser(42 + id)
+        }
+    }
 }
 
 @Composable
 fun complicationSelectRow(
-  chooseComplication: (Int) -> Unit,
-  addComplication: () -> Unit,
-  removeComplication: () -> Unit,
-  ids: Int
+    chooseComplication: (Int) -> Unit,
+    // TODO: update activity state
+    addComplication: () -> Unit,
+    removeComplication: () -> Unit,
+    maxId: Int
 ) {
-  val myMod = Modifier
+    val (complicationCount, setComplicationCount) = remember { mutableStateOf(maxId) }
+    val myMod = Modifier
       .background(color = Color.Black)
       .fillMaxWidth()
       .fillMaxSize()
 
-  Column(
-    modifier = myMod,
-    verticalArrangement = Arrangement.Center,
-    horizontalAlignment = Alignment.CenterHorizontally
-  ) {
-    Text("Chose Complications", color = Color.White)
-    LazyRow {
-      items(ids) { id ->
-        complicationChooserButton(
-          selectComplicationHandler = chooseComplication,
-          removeComplication,
-          id = id
-        )
-      }
-      item {
-        Button(onClick = addComplication) {
-          Text("+")
+    Column(
+        modifier = myMod,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Chose Complications", color = Color.White)
+        LazyRow {
+            items(complicationCount) { id ->
+                complicationChooserButton(
+                    selectComplicationHandler = chooseComplication,
+                    removeComplication = { setComplicationCount(complicationCount - 1) },
+                    id = id
+                )
+            }
+            item {
+                Button(onClick = { setComplicationCount(complicationCount + 1) }) {
+                    Text("+")
+                }
+            }
         }
-      }
     }
-  }
 }
 
 @Composable
 fun complicationChooserButton(
-  selectComplicationHandler: (Int) -> Unit,
-  removeComplication: () -> Unit,
-  id: Int
+    selectComplicationHandler: (Int) -> Unit,
+    removeComplication: () -> Unit,
+    id: Int
 ) {
-  val chooseButtonModifier = Modifier.width(50.dp)
-  val removeFieldModifier = Modifier
+    val chooseButtonModifier = Modifier.width(50.dp)
+    val removeFieldModifier = Modifier
       .width(30.dp)
       .height(30.dp)
-  val chooseButtonColor = ButtonDefaults.buttonColors(containerColor = Color.Blue)
-  val removeFieldColor = ButtonDefaults.buttonColors(containerColor = Color.Yellow)
-  Column(modifier = chooseButtonModifier, horizontalAlignment = Alignment.CenterHorizontally) {
-    Button(
-      onClick = { selectComplicationHandler(id) },
-      modifier = chooseButtonModifier,
-      colors = chooseButtonColor,
-    ) {
-      Text("$id", textAlign = TextAlign.Center)
+    val chooseButtonColor = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+    val removeFieldColor = ButtonDefaults.buttonColors(containerColor = Color.Yellow)
+    Column(modifier = chooseButtonModifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(
+            onClick = { selectComplicationHandler(id) },
+            modifier = chooseButtonModifier,
+            colors = chooseButtonColor,
+        ) {
+            Text("$id", textAlign = TextAlign.Center)
+        }
+        Button(
+            onClick = removeComplication,
+            modifier = removeFieldModifier,
+            colors = removeFieldColor,
+            contentPadding = PaddingValues(0.dp)
+        ) {
+            Text(text = "❌", fontSize = 1.em, textAlign = TextAlign.Center)
+        }
     }
-    Button(
-      onClick = removeComplication,
-      modifier = removeFieldModifier,
-      colors = removeFieldColor,
-      contentPadding = PaddingValues(0.dp)
-    ) {
-      Text(text = "❌", fontSize = 1.em, textAlign = TextAlign.Center)
-    }
-  }
 }
 
 
 @Preview(device = Devices.WEAR_OS_LARGE_ROUND)
 @Composable
 fun preview() {
-  var (complicationCount, setComplicationCount) = remember { mutableIntStateOf(3) }
-  val dummyCallback: (Int) -> Unit = { id -> println("The ID is: $id") }
-  complicationSelectRow(
-    dummyCallback,
-    { setComplicationCount(complicationCount + 1) },
-    { setComplicationCount(complicationCount - 1) },
-    complicationCount
-  )
+    var (complicationCount, setComplicationCount) = remember { mutableStateOf(3) }
+    val dummyCallback: (Int) -> Unit = { id -> println("The ID is: $id") }
+    complicationSelectRow(
+        dummyCallback,
+        { setComplicationCount(complicationCount + 1) },
+        { setComplicationCount(complicationCount - 1) },
+        complicationCount
+    )
 }
