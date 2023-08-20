07c255c877e042a791356ed9be625d9857bcc42a
niilz <niilz@outlook.de>
2023-08-20 13:35:31 +0200

    start implementing configStateHolder

--- app/src/main/java/de/niilz/wearos/watchface/bttf/editor/ConfigStateHolder.kt
+++ app/src/main/java/de/niilz/wearos/watchface/bttf/editor/ConfigStateHolder.kt
@@ -1,16 +1,35 @@
 package de.niilz.wearos.watchface.bttf.editor
 
-// FIXME: Replace with CurrentUserStyleRepository
-class ConfigStateHolder {
-  companion object {
-    var complicationCount = 0
+import androidx.activity.ComponentActivity
+import androidx.wear.watchface.editor.EditorSession
+import kotlinx.coroutines.CoroutineScope
 
-    fun incrementComplicationCount() {
-      complicationCount += 1
-    }
+class ConfigStateHolder(private val scope: CoroutineScope, activity: ComponentActivity) {
 
-    fun decrementComplicationCount() {
-      complicationCount -= 1
+    private lateinit var editorSession: EditorSession
+
+    /*
+    val uiState: StateFlow<Int> = flow<Int> {
+        editorSession = EditorSession.createOnWatchEditorSession(activity)
+        val userSettings = editorSession.userStyleSchema.userStyleSettings
+        assert(userSettings.size == 1)
+        val complicationSetting = userSettings[0]
+        assert(complicationSetting.id.value == ("slot-count"))
+        val complicationCount = complicationSetting.options[0]
+      emit(complicationCount.)
+
+    }.stateIn(scope + Dispatchers.Main.immediate, SharingStarted.Eagerly, 0)
+     */
+
+    companion object {
+        var complicationCount = 0
+
+        fun incrementComplicationCount() {
+            complicationCount += 1
+        }
+
+        fun decrementComplicationCount() {
+            complicationCount -= 1
+        }
     }
-  }
 }
