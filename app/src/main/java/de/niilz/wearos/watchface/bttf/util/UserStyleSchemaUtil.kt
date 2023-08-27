fe927d9d8e9068a89af219e1a6b3a23d0df4b512
niilz <niilz@outlook.de>
2023-08-27 17:20:30 +0200

    Do not add a zero-th complication

--- app/src/main/java/de/niilz/wearos/watchface/bttf/util/UserStyleSchemaUtil.kt
+++ app/src/main/java/de/niilz/wearos/watchface/bttf/util/UserStyleSchemaUtil.kt
@@ -8,7 +8,7 @@ import de.niilz.wearos.watchface.bttf.R
 
 // FIXME: We probably need to add a ConfigStateHolder aswell, use it in the editor and listen for updates
 fun createBttfComplicationUserStyleSchema(applicationContext: Context): UserStyleSchema {
-  val optionsConfig = (1..7).map { createComplicationSlotOptions(it) }
+  val optionsConfig = (0..7).map { createComplicationSlotOptions(it) }
   val complicationSettings = UserStyleSetting.ComplicationSlotsUserStyleSetting(
     id = UserStyleSetting.Id("complication-slot-settings"),
     resources = applicationContext.resources,
@@ -23,17 +23,21 @@ fun createBttfComplicationUserStyleSchema(applicationContext: Context): UserStyl
 }
 
 private fun createComplicationSlotOptions(slotCounts: Int): UserStyleSetting.ComplicationSlotsUserStyleSetting.ComplicationSlotsOption {
+  val complicationOverlays = (1..slotCounts).map { slotId ->
+    createComplicationSlotOverlay(slotId)
+  }
   return UserStyleSetting.ComplicationSlotsUserStyleSetting.ComplicationSlotsOption(
-    UserStyleSetting.Option.Id("slot-counts-$slotCounts"), "Slot-Counts-$slotCounts", null,
-    (0..slotCounts).map { slotId ->
-      createComplicationSlotOverlay(slotId)
-    }
+    UserStyleSetting.Option.Id("slot-counts-${complicationOverlays.size}"),
+    "Slot-Counts-${complicationOverlays.size}",
+    null,
+    complicationOverlays
   )
 }
 
 private fun createComplicationSlotOverlay(id: Int): UserStyleSetting.ComplicationSlotsUserStyleSetting.ComplicationSlotOverlay {
+  // Log.d(TAG, "Slot-Overlay-ID: $id")
   return UserStyleSetting.ComplicationSlotsUserStyleSetting.ComplicationSlotOverlay(
-    complicationSlotId = id,
-    enabled = false,
+    complicationSlotId = 100 + id,
+    enabled = true,
   )
 }
